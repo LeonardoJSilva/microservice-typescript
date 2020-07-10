ca172171377b584748e50e4b4f19f8aa60a333d5
leonardo silva <leonsilva700@gmail.com>
2020-07-10 00:34:03 -0300

    refac repository e controller

--- src/controller/supplier.ts
+++ src/controller/supplier.ts
@@ -3,32 +3,43 @@ import Supplier from "../database/database";
 import {dataValidator} from "../utils/dataValidator";
 import {SupplierRepository} from "../database/supplierRepository";
 import {$log} from "@tsed/logger";
-import {type} from "os";
 
 $log.name = "CONTROLLER";
 
 export let allSuppliers = async (req: Request, res: Response) => {
     $log.name = "CONTROLLER";
-    $log.info("Get all suppliers")
+    $log.info("Get all suppliers");
     const response = await SupplierRepository.findAll();
     res.send(response)
 };
 
 export let getSupplier = async (req: Request, res: Response) => {
     $log.name = "CONTROLLER";
-    $log.info(`Get supplier with code ${Number(req.params.code)}`)
-    const response = await SupplierRepository.findByCode(Number(req.params.code));
-    res.send(response)
+    $log.info(`Get supplier with code ${Number(req.params.code)}`);
+    const supplier = await SupplierRepository.findByCode(Number(req.params.code));
+    if (supplier == null || typeof supplier != "object") {
+        $log.info("Supplier not found")
+        res.statusCode = 404;
+        res.send("Supplier not found");
+    } else {
+        $log.info({"name": supplier.name, "code": supplier.code, "cnpj": supplier.cnpj})
+        res.send({"name": supplier.name, "code": supplier.code, "cnpj": supplier.cnpj});
+    }
 };
 
 export let deleteSupplier = async (req: Request, res: Response) => {
     $log.name = "CONTROLLER";
-    $log.info(`Delete supplier with code ${Number(req.params.code)}`)
-    let response = await SupplierRepository.findByCode(Number(req.params.code));
-    if (typeof(response) == "object"){
-        response = await SupplierRepository.deleteByCode(Number(req.params.code));
+    $log.info(`Delete supplier with code ${Number(req.params.code)}`);
+    let supplier = await SupplierRepository.findByCode(Number(req.params.code));
+    if (supplier == null || typeof supplier != "object") {
+        $log.info("Supplier not found");
+        res.statusCode = 404;
+        res.send("Supplier not found");
+    } else {
+        await SupplierRepository.deleteByCode(Number(req.params.code));
+        res.statusCode = 204;
+        res.send();
     }
-    res.send(response)
 };
 
 export let addSupplier = async (req: Request, res: Response) => {
@@ -45,12 +56,15 @@ export let addSupplier = async (req: Request, res: Response) => {
             res.send({"name": supplier.name, "code": supplier.code, "cnpj": supplier.cnpj});
         } else if (err.name == "ValidationError") {
             $log.info(err.message);
+            res.statusCode = 422;
             res.send(err.message);
         } else if (err.name == "MongoError") {
             $log.info("Code or cnpj already exists in database");
+            res.statusCode = 422;
             res.send("Code or cnpj already exists in database");
         } else {
-            $log.error(err)
+            $log.error(err);
+            res.statusCode = 500;
             res.send(err);
         }
     });
--- src/database/supplierRepository.ts
+++ src/database/supplierRepository.ts
@@ -6,59 +6,25 @@ $log.level = "debug";
 $log.name = "SUPPLIER REPOSITORY";
 
 export class SupplierRepository {
-    static async findByCode(code: number) {
+    static async findByCode(code: number): Promise<ISupplierDatabase | null> {
         $log.name = "SUPPLIER REPOSITORY";
         const filter: FilterQuery<ISupplierDatabase> = {"code": code, "deleted": false};
-        let response;
-        await Supplier.findOne(filter, (err: any, supplier: DocumentDefinition<ISupplierDatabase>) => {
-            if (err) {
-                response = err;
-            } else if (supplier == null) {
-                $log.info("Supplier not found")
-                response = "Supplier not found";
-            } else {
-                $log.info({"name": supplier.name, "code": supplier.code, "cnpj": supplier.cnpj})
-                response = {"name": supplier.name, "code": supplier.code, "cnpj": supplier.cnpj};
-            }
-        });
-        return response;
+        return Supplier.findOne(filter, {_id: 0, "__v": 0});
     }
 
     static async findByCodes(codes: Array<number>): Promise<ISupplierDatabase[]>{
         $log.name = "SUPPLIER REPOSITORY";
-        let response: ISupplierDatabase[];
-        response = await Supplier.find({"deleted": false, "code": { "$in" : codes}}, {_id: 0, "__v": 0});
-        return response;
-
+        return Supplier.find({"deleted": false, "code": { "$in" : codes}}, {_id: 0, "__v": 0});
     }
 
-    static async deleteByCode(code: number) {
+    static async deleteByCode(code: number){
         $log.name = "SUPPLIER REPOSITORY";
         const filter: FilterQuery<ISupplierDatabase> = {"code": code, "deleted": false};
-        let response;
-        await Supplier.updateOne(filter, {"deleted": true}, (err: any) => {
-            if (err) {
-                $log.error(err)
-                response = err;
-            } else {
-                $log.info(`Successfully deleted supplier with code ${code}`);
-                response = `Successfully deleted supplier with code ${code}`
-            }
-        });
-        return response;
+        await Supplier.updateOne(filter, {"deleted": true});
     }
 
-    static async findAll() {
+    static async findAll() : Promise<ISupplierDatabase[]> {
         $log.name = "SUPPLIER REPOSITORY";
-        let response;
-        await Supplier.find({"deleted": false}, {_id: 0, "__v": 0}, (err: any, suppliers: DocumentDefinition<ISupplierDatabase>[]) => {
-            if (err) {
-                $log.error(err)
-                response = err;
-            } else {
-                response = suppliers;
-            }
-        });
-        return response;
+        return Supplier.find({"deleted": false}, {_id: 0, "__v": 0});
     }
 }
